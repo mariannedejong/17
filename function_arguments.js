04b95855dd73a1cb2e2ea01b7a8e7f8dd4b867c1
mariannedejong <89845904+mariannedejong@users.noreply.github.com>
2021-11-10 00:09:48 +0100

    Add files via upload

---
+++ function_arguments.js
@@ -0,0 +1,6 @@
+let paintWall = function (whichWall, colour){
+    console.log("This " + whichWall + " wall has been painted " + colour);
+}
+whichWall = "south-east";
+colour = "brown";
+paintWall(whichWall, colour);
